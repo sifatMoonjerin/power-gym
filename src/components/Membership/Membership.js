cdd2b2a9f6bb00ff47c58348a9fe77865c46ee57
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-01 20:48:38 +0600

    PayPalButton added

--- src/components/Membership/Membership.js
+++ src/components/Membership/Membership.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PaypalBox from '../PaypalBox/PaypalBox';
 
 const Membership = () => {
     return (
         <div>
             <h1>Membership Page</h1>
+            <PaypalBox/>
         </div>
     );
 };
---
+++ src/components/PaypalBox/PaypalBox.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { PayPalButton } from 'react-paypal-button-v2';
+
+const PaypalBox = () => {
+    return (
+        <div>
+            <PayPalButton
+                amount="0.01"
+                shippingPreference="NO_SHIPPING" 
+                onSuccess={(details, data) => {
+                    alert("Transaction completed by " + details.payer.name.given_name);
+            
+                    // OPTIONAL: Call your server to save the transaction
+                    // return fetch("/paypal-transaction-complete", {
+                    //     method: "post",
+                    //     body: JSON.stringify({
+                    //     orderID: data.orderID
+                    //     })
+                    // });
+                }}
+                options={{
+                    clientId: "AVc7YQV6yOdALphh5g4tW7XphlCV9Lee0xQ3FYhstdgBpRZaI7dfv9Zz1PM5d6ehIBn8SReUROCQ9i3W"
+                }}
+            />
+        </div>
+    );
+};
+
+export default PaypalBox;
