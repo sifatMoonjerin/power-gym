c4a70e700de2027f51d89b39afd5cf035f8e0ace
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-01 15:50:25 +0600

    router integrated

---
+++ src/components/Membership/Membership.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Membership = () => {
+    return (
+        <div>
+            <h1>Membership Page</h1>
+        </div>
+    );
+};
+
+export default Membership;
