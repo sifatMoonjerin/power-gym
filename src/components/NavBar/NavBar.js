028e906f6cddd1e809e43b575fe7f3a35c5f5eed
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-02 14:41:15 +0600

    navbar added

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavBar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <Link className="navbar-brand" to="/">Power Gym</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div className="navbar-nav">
+                    <Link className="nav-item nav-link" to="/">Home</Link>
+                    <Link className="nav-item nav-link" to="/classes">Our Classes</Link>
+                    <Link className="nav-item nav-link" to="/pricing">Pricing</Link>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default NavBar;
