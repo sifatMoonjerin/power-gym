d5422f90b29e66c9e7f40e37ab71d0b3ff29a0dc
sifatMoonjerin <moonjerin.sifat@gmail.com>
2020-06-03 15:54:59 +0600

    home page added

--- src/components/TopBanner/TopBanner.js
+++ src/components/TopBanner/TopBanner.js
@@ -1,9 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const TopBanner = () => {
     return (
-        <div className="top-banner">
-            
+        <div className="top-banner ">
+            <div className='row'>
+            <div className='col-md-6 p-5'>
+                <h1 className='text-light'>THE BEST FITNESS STUDIO IN TOWN</h1>
+                <p className='text-light'>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
+                  sed do eiusmod tempor incididunt ut labore et dolore 
+                  magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                  exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. Duis aute irure dolor in reprehenderit in 
+                  voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                </p>
+                <Link to='/pricing'>
+                    <button className='btn btn-warning'>JOIN US</button>
+                </Link>
+            </div>
+            </div>
         </div>
     );
 };
